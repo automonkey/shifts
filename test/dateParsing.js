5da2ee77644b7357c5608025523fa7e02f08aeba
Will Benyon <git@benyon.io>
2016-10-24 13:31:26 +0100

    tmp

--- test/dateParsing.js
+++ test/dateParsing.js
@@ -5,8 +5,13 @@ const parser = require('../lib/dateParser');
 describe('date parser', function() {
 
   it('should parse date', function() {
-    var eventDateRange = parser.processEntry(
+
+    var res = parser.processEntry(
         'Sept 24 2016 from 13:12:21 to 14:34:43');
+
+    expect(res.errors).to.be.null;
+
+    var eventDateRange = res.date;
 
     expect(eventDateRange).to.have.property('from');
     var from = eventDateRange.from;
@@ -25,6 +30,19 @@ describe('date parser', function() {
     expect(to.getHours()).to.equal(14);
     expect(to.getMinutes()).to.equal(34);
     expect(to.getSeconds()).to.equal(43);
+  });
+
+  it('should error if empty input', function() {
+    const res = parser.processEntry('');
+    expect(res.date).to.be.null;
+    expect(res.errors.indexOf('No event data supplied') > -1);
+  });
+
+  it('should error if invalid date', function() {
+    const res = parser.processEntry('some garbage');
+    expect(res.date).to.be.null;
+    expect(res.errors.indexOf(
+      "Are you speaking Klingon? I don't understand 'some garbage'" > -1));
   });
 
 });
