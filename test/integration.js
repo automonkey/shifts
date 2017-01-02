c4aada066c655fea207907af851a63fc284b54bd
Will Benyon <git@benyon.io>
2017-01-02 13:34:41 +0000

    Label integration tests
    Useful to differentiate in mocha output.

--- test/integration.js
+++ test/integration.js
@@ -4,44 +4,48 @@ const expect = require('chai').expect;
 
 const app = require('../lib/app');
 
-describe('date entry processing', function() {
+describe('integration tests', () => {
 
-  it('should return ics data', done => {
+  describe('date entry processing', () => {
 
-    const expectedIcsData =
-      "BEGIN:VCALENDAR\n" +
-      "VERSION:2.0\n" +
-      "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n" +
-      "BEGIN:VEVENT\n" +
-      "UID:uid1@example.com\n" +
-      "DTSTAMP:19970714T170000Z\n" +
-      "ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com\n" +
-      "DTSTART;TZID=Europe/London:20161022T113621\n" +
-      "DTEND;TZID=Europe/London:20161022T123443\n" +
-      "SUMMARY:Bastille Day Party\n" +
-      "END:VEVENT\n" +
-      "END:VCALENDAR";
+    it('should return ics data', done => {
 
-    request(app())
-      .post('/')
-      .type('form')
-      .send({dateEntry: 'October 22 2016 from 11:36:21 to 12:34:43'})
-      .expect(200)
-      .expect(expectedIcsData, done);
+      const expectedIcsData =
+        "BEGIN:VCALENDAR\n" +
+        "VERSION:2.0\n" +
+        "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n" +
+        "BEGIN:VEVENT\n" +
+        "UID:uid1@example.com\n" +
+        "DTSTAMP:19970714T170000Z\n" +
+        "ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com\n" +
+        "DTSTART;TZID=Europe/London:20161022T113621\n" +
+        "DTEND;TZID=Europe/London:20161022T123443\n" +
+        "SUMMARY:Bastille Day Party\n" +
+        "END:VEVENT\n" +
+        "END:VCALENDAR";
+
+      request(app())
+        .post('/')
+        .type('form')
+        .send({dateEntry: 'October 22 2016 from 11:36:21 to 12:34:43'})
+        .expect(200)
+        .expect(expectedIcsData, done);
+    });
+
+    it('should report invalid date entry', done => {
+      request(app())
+        .post('/')
+        .type('form')
+        .send({dateEntry: 'something-invalid'})
+        .expect(400)
+        .expect(res => {
+          const html = cheerio.load(res.text);
+          expect(html('input#dateEntry').val()).to.equal('something-invalid');
+          expect(html('span#error-msg').text()).to.equal("What kind of a time is 'something-invalid'?!");
+        })
+        .end(done)
+    })
+
   });
-
-  it('should report invalid date entry', done => {
-    request(app())
-      .post('/')
-      .type('form')
-      .send({dateEntry: 'something-invalid'})
-      .expect(400)
-      .expect(res => {
-        const html = cheerio.load(res.text);
-        expect(html('input#dateEntry').val()).to.equal('something-invalid');
-        expect(html('span#error-msg').text()).to.equal("What kind of a time is 'something-invalid'?!");
-      })
-      .end(done)
-  })
 
 });
