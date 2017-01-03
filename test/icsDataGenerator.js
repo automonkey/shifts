b532aa869c0b8cd312ab2867b5f7e42816f83c7b
Will Benyon <git@benyon.io>
2017-01-03 20:41:50 +0000

    Generate unique ids for events
    Replace hardcoded event id with generated id.

--- test/icsDataGenerator.js
+++ test/icsDataGenerator.js
@@ -1,4 +1,6 @@
-const expect = require('chai').expect;
+const expect = require('chai').expect
+    , mockery = require('mockery')
+    , sinon = require('sinon');
 
 const icsDataGenerator = require('../lib/icsDataGenerator');
 
@@ -30,10 +32,59 @@ describe('ics data generator', () => {
       datetime: '20161229T013500'});
   });
 
+  describe('event id generation', () => {
+
+    let uuidGeneratorStub;
+    let stubbedIcsDataGenerator;
+
+    before(() => {
+      mockery.enable({
+        warnOnReplace: false,
+        warnOnUnregistered: false,
+        useCleanCache: true
+      });
+
+      uuidGeneratorStub = sinon.stub();
+
+      mockery.registerMock('./uuidGenerator', uuidGeneratorStub);
+      stubbedIcsDataGenerator = require('../lib/icsDataGenerator');
+    });
+
+    after(() => {
+      mockery.disable();
+    });
+
+    it('should generate events with unique ids', () => {
+      uuidGeneratorStub.returns('some-random-event-id');
+
+      const icsData = stubbedIcsDataGenerator.generate(
+        {
+          from: new Date(2016, 11, 29, 1, 31),
+          to: new Date(2016, 11, 29, 1, 35)
+        }
+      );
+
+      const events = extractEvents(icsData);
+      expect(events).to.have.length.of(1);
+
+      const eventIds = extractEventIds(events[0]);
+      expect(eventIds).to.have.length.of(1);
+      expect(eventIds[0]).to.equal('some-random-event-id@shifts.benyon.io');
+    });
+
+  });
+
 });
 
 function extractEvents(icsData) {
   return icsData.match(/^BEGIN:VEVENT[\s\S]+?END:VEVENT$/gm);
+}
+
+function extractEventIds(icsEventData) {
+  return matchAndExtractCaptureGroups(icsEventData, /^UID:(.*)$/gm)
+    .map(entry => {
+      return entry[0];
+    });
 }
 
 function extractStartDates(icsEventData) {
--- test/integration.js
+++ test/integration.js
@@ -10,26 +10,30 @@ describe('integration tests', () => {
 
     it('should return ics data', done => {
 
-      const expectedIcsData =
-        "BEGIN:VCALENDAR\n" +
-        "VERSION:2.0\n" +
-        "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n" +
-        "BEGIN:VEVENT\n" +
-        "UID:uid1@example.com\n" +
-        "DTSTAMP:19970714T170000Z\n" +
-        "ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com\n" +
-        "DTSTART;TZID=Europe/London:20161022T113621\n" +
-        "DTEND;TZID=Europe/London:20161022T123443\n" +
-        "SUMMARY:Bastille Day Party\n" +
-        "END:VEVENT\n" +
-        "END:VCALENDAR";
+      const icsDataRegex =
+        "^BEGIN:VCALENDAR\\n" +
+        "VERSION:2.0\\n" +
+        "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\\n" +
+        "BEGIN:VEVENT\\n" +
+        "UID:.+@shifts.benyon.io\\n" +
+        "DTSTAMP:19970714T170000Z\\n" +
+        "ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com\\n" +
+        "DTSTART;TZID=Europe/London:20161022T113621\\n" +
+        "DTEND;TZID=Europe/London:20161022T123443\\n" +
+        "SUMMARY:Bastille Day Party\\n" +
+        "END:VEVENT\\n" +
+        "END:VCALENDAR$";
 
       request(app())
         .post('/')
         .type('form')
         .send({dateEntry: 'October 22 2016 from 11:36:21 to 12:34:43'})
         .expect(200)
-        .expect(expectedIcsData, done);
+        .expect(res => {
+          const re = new RegExp(icsDataRegex, 'g');
+          expect(res.text).to.match(re);
+        })
+        .end(done);
     });
 
     it('should report invalid date entry', done => {
